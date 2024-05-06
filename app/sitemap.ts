8692f74879ac6eab7eeaa775aee5bc6396032d38
mohamedzouine827 <mohamedzouine827@gmail.com>
2024-05-06 18:40:00 +0100

    8

---
+++ app/sitemap.ts
@@ -0,0 +1,19 @@
+export default async function sitemap() {
+    return [{
+        url : "https://nizar-abdelkarim.vercel.app",
+        lastModified : new Date(),
+    },
+    {
+        url : "https://nizar-abdelkarim.vercel.app/work",
+        lastModified : new Date(),
+    },
+    {
+        url : "https://nizar-abdelkarim.vercel.app/about",
+        lastModified : new Date(),
+    },
+    {
+        url : "https://nizar-abdelkarim.vercel.app/contact",
+        lastModified : new Date(),
+    }]
+    
+}
